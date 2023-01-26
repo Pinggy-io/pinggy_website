0dd32b222894db0f68c88ccb0156e00dea081568
Bishakh Ghosh <ghoshbishakh@gmail.com>
2023-01-27 02:27:46 +0530

    Update header part

--- static/js/index.js
+++ static/js/index.js
@@ -1,25 +1,73 @@
-let portinput = document.getElementById('portform');
-let  portcommand = document.getElementById('portcommand');
+// ---------- try it yourself form ------
 
-portinput.addEventListener('keyup', function(){
-    portcommand.value = "ssh -p 443 -R0:localhost:" +  (portinput.value || "8000") + " a.pinggy.io";
+$('#techselect').on('change', function() {
+    var label = $('option:selected', this).data('label');
+    var command = $('option:selected', this).data('command');
+    var port = $('option:selected', this).data('port');
+
+    if(label){
+        if(label != $('#tryityourselflabel').text()){
+            $('#tryityourselflabel').animate({'opacity': 0}, 400, function(){
+                $(this).text(label).animate({'opacity': 1}, 400);    
+            });
+        }
+        $('#tryityourselflabel').show('slow');
+    }
+    else {
+        $('#tryityourselflabel').hide('slow');
+    }
+
+    if(command){
+        $('#tryityourselfprecommand').val(command);
         
-})
+        $('#tryityourselfprecommand').css("visibility", "visible");
+        $('#copybutton2').css("visibility", "visible");
 
-function copytoclipboard() {
+        $('#tryityourselfprecommand').animate({opacity: 1}, 1200);
+        $('#copybutton2').animate({opacity: 1}, 1200);
+    
+    } else {
+        $('#tryityourselfprecommand').animate({opacity: 0}, 1200);
+        $('#copybutton2').animate({opacity: 0}, 1200);
+        $('#tryityourselfprecommand').css("visibility", "hidden");
+        $('#copybutton2').css("visibility", "hidden");
+    }
+
+    $('#portform').val(port).trigger("input");;
+    
+});
+
+
+// ---------- ------------
+
+$( "#portform" ).on("input", function() {
+    $( "#portcommand" ).val("ssh -p 443 -R0:localhost:" +  ($( "#portform" ).val() || "8000") + " a.pinggy.io");
+});
+
+function copytoclipboard(element, inputselector, amplitudemsg) {
+    var portcommand = $( inputselector )[0];
     // Get the text field
     portcommand.select(); 
     portcommand.setSelectionRange(0, 99999); // For mobile devices
-  
      // Copy the text inside the text field
     navigator.clipboard.writeText(portcommand.value);
-
     var amplitudeEvent = "SSH url copy button clicked";
     var eventProperties = {
         "url": portcommand.value
     };
     amplitude.getInstance().logEvent(amplitudeEvent, eventProperties);
+    $(element).children("i").removeClass('bi-clipboard');
+    $(element).children("i").addClass('bi-check');
+    setTimeout(
+        function(element) 
+        {
+          $(element).children("i").removeClass('bi-check');
+          $(element).children("i").addClass('bi-clipboard');
+        }, 1000, element);
+    
+
 }
+
 
 // Download button system auto detect:
 
